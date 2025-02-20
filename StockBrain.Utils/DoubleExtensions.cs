2aec2bbe149bab9a71178b31abb0369440dd61f9
GlauberCoder <glauber.almeida@supplybrain.ai>
2025-02-19 21:28:06 -0300

    fix:Recomendação quando carteira está vazia

--- StockBrain.Utils/DoubleExtensions.cs
+++ StockBrain.Utils/DoubleExtensions.cs
@@ -2,8 +2,8 @@
 
 public static class DoubleExtensions
 {
-    public static double ToPrecision(this double number, int precision = 2) => double.IsNaN(number) ? 0 : (double)Math.Round((decimal)number, precision);
-    public static double ToPercentage(this double number, int precision = 0) => double.IsNaN(number) ? 0 : (double)Math.Round((decimal)number * 100, precision);
+    public static double ToPrecision(this double number, int precision = 2) => double.IsNaN(number) || double.IsInfinity(number) ? 0 : (double)Math.Round((decimal)number, precision);
+    public static double ToPercentage(this double number, int precision = 0) => double.IsNaN(number) || double.IsInfinity(number) ? 0 : (double)Math.Round((decimal)number * 100, precision);
     public static string PercentageFormat(this double number, int precision = 0) => $"{number.ToPercentage(precision)}%";
 	public static string MonetaryFormat(this double value) => String.Format(new System.Globalization.CultureInfo("pt-Br"), "{0:C}", value);
 	public static string MonetaryThousandFormat(this double value) => $"{(value/1000).ToPrecision(0)} k";
