1749e577d499e3027f687d9a6d0deb946e3f1ce7
GlauberCoder <glauber.almeida@supplybrain.ai>
2025-02-28 00:19:33 -0300

    testando commit

--- StockBrain.Domain.Models/Portfolio.cs
+++ StockBrain.Domain.Models/Portfolio.cs
@@ -4,6 +4,7 @@ namespace StockBrain.Domain.Models;
 
 public class Portfolio : BaseEntity
 {
+	//TODO:Vai
 	public required string Name { get; init; }
 	public required double Total { get; init; }
 	public required IEnumerable<PortfolioAssetDetail> Assets { get; set; }
